fb27a11dad7277c922a6f88659e2f2bc11f00d7e
carlfrank <carl-frank7@hotmail.com>
2023-12-15 17:40:29 -0500

    Updating hbnb filters

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,27 +1,35 @@
-#!/usr/bin/python3
-"""Starts a Flask web app
-Listens to port 5000 on 0.0.0.0
-"""
-from models import storage
-from flask import Flask, render_template
+# Your Flask application script
 
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from os import environ
 
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def hbnb_filters():
-    """Displays HBNB filters on rendered page"""
-    states = storage.all('State').values()
-    amenities = storage.all('Amenity').values()
-    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
-
-
 @app.teardown_appcontext
-def teardown(exception):
-    """Remove session"""
+def teardown_appcontext(self):
     storage.close()
 
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        cities = storage.all(City).values()
+    else:
+        cities = {}
+
+    dropdown_data = [{'value': state.id, 'label': state.name}
+                     for state in states]
+
+    return render_template('10-hbnb_filters.html', states=states, cities=cities, amenities=amenities, dropdown_data=dropdown_data)
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
