dd64060d10506f9277cfeaedb661d267b9ca03ce
carlfrank <carl-frank7@hotmail.com>
2023-12-15 12:21:40 -0500

    Adding imports states 7-

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
-from models import storage
+""" This module starts a Flask web application """""
 from flask import Flask
 from flask import render_template
-
+from models import storage
+from models.state import State
+""" Flask class and render_template method"""""
 app = Flask(__name__)
-
-
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(self):
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+@app.route('/states_list')
+def states_list():
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
