a5b100464e3cd903c1b24b2f0efae9fd48f52900
carlfrank <carl-frank7@hotmail.com>
2023-12-15 09:55:26 -0500

    7-states, changing code

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,30 +5,33 @@ Starts a Flask web app
 
 
 from flask import Flask, render_template
-from models import *
-from models import storage
-app = Flask(__name__)
+from models import storage, State
+import sys
 
+app = Flask(__name__)
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """
-    renders HTML page with the states list
+    Renders HTML page with the states list
     """
-    states = list(storage.all(State).values())
-    return render_template('7-states_list.html', states=states)
-
+    try:
+        states = list(storage.all(State).values())
+        states.sort(key=lambda x: x.name)
+        return render_template('7-states_list.html', states=states)
+    except Exception as e:
+        print(f"Error: {e}", file=sys.stderr)
+        return "An error occurred", 500
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """
-    closes the storage on teardown
+    Closes the storage on teardown
     """
     storage.close()
 
-
 if __name__ == '__main__':
     """
-    run when invoked
+    Run when invoked
     """
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000, debug=True)
